d3dc7be5b2ed22440c05ac3943031837a374b60b
CyberMindX-AI <darasimimakinde442@gmail.com>
2025-09-30 14:35:24 +0100

    chanes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -43,13 +42,10 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
+      jsxImportSource: 'react',
+      tsDecorators: true,
     }),
-    ...(mode === "development" ? [componentTagger()] : []),
-    visualizer()
+    ...(mode === "development" ? [componentTagger()] : [])
   ],
   resolve: {
     alias: {
